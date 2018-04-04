2230d53852d2d796c6e5399e12bdac5c07b52903
niezhiliang <1132263176@qq.com>
2018-04-04 12:04:54 +0800

    m

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import App from '../App'
 import index from '../index'
-import Label from '../label'
 
 Vue.use(Router)
 
@@ -16,11 +15,6 @@ export default new Router({
           path: '/',
           component: index,
           name: 'index'
-        },
-        {
-          path: '/l',
-          component: Label,
-          name: 'l'
         }
       ]
     }
