24091f0cf593df515583299a10745dc92dca6465
niezhiliang <1132263176@qq.com>
2018-04-04 12:04:05 +0800

    m

--- src/index.vue
+++ src/index.vue
@@ -53,7 +53,7 @@
               <template slot-scope="scope">
                 <el-button
                   size="mini"
-                  @click="updata_data(scope.$index, scope.row)">编辑</el-button>
+                  @click="init_form(scope.$index, scope.row)">编辑</el-button>
                 <el-button
                   size="mini"
                   type="danger"
@@ -69,12 +69,12 @@
         :visible.sync="editDialogVisible"
         width="30%"
         center>
-        <el-input placeholder="编辑姓名"></el-input>
-        <el-input placeholder="编辑性别"></el-input>
-        <el-input placeholder="编辑年龄"></el-input>
+        姓名：<el-input placeholder="编辑姓名" v-model="upstu.name"></el-input>
+        性别：<el-input placeholder="编辑性别" v-model="upstu.sex"></el-input>
+        年龄：<el-input placeholder="编辑年龄" v-model="upstu.age"></el-input>
         <span slot="footer" class="dialog-footer">
           <el-button @click="editDialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="centerDialogVisible = false;">确 定</el-button>
+          <el-button type="primary" @click="update_date">确 定</el-button>
         </span>
     </el-dialog>
   </div>
@@ -91,7 +91,13 @@
           sex: '',
           age: ''
         },
-        editDialogVisible:true,
+        upstu: {
+          id: '',
+          name: '',
+          sex: '',
+          age: ''
+        },
+        editDialogVisible:false,
         db: '',
         deleteid: '',
         self: this,
@@ -104,17 +110,14 @@
       this.get_data()
     },
     methods: {
-      open5(index,row) {
-        this.$alert('<div style="text-align: center">姓名：<input type="text" style="margin-bottom: 10px" :value="row.name"/><br>性别：<input type="text" style="margin-bottom: 10px"/><br>年龄：<input type="text"/></div>', '信息修改', {
-          dangerouslyUseHTMLString: true
-        })
-      },
       create_websql: function () {
         //创建名为mydb的数据库             版本    描述      大小
           this.db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
         //创建一个名为STUDENT的表,如果存在则不会创建
           this.db.transaction(function (tx) {
             tx.executeSql('CREATE TABLE IF NOT EXISTS STUDENT (id unique, name, sex, age)');
+            tx.executeSql('INSERT INTO STUDENT (id, name, sex, age) VALUES (?, ?, ?, ?)',[1522813443000,'苏雨', '男', 20]);
+            tx.executeSql('INSERT INTO STUDENT (id, name, sex, age) VALUES (?, ?, ?, ?)',[1522813443001,'苏苏', '女', 18]);
           });
       },
       //向student表中动态添加一条数据  这个操作跟我们平常的操作数据库一样
@@ -177,15 +180,23 @@
         });
 
       },
-      updata_data: function (index,row) {
+      init_form: function (index,row) {
         this.editDialogVisible = true;
+        this.upstu.name = row.name
+        this.upstu.sex = row.sex
+        this.upstu.age = row.age
+        this.upstu.id = row.id
+      },
+      update_date: function () {
+        let name = this.upstu.name
+        let sex = this.upstu.sex
+        let age = this.upstu.age
+        let id = this.upstu.id
         this.db.transaction(function (tx) {
-          tx.executeSql('UPDATE STUDENT SET name = ?,sex = ?,age=? WHERE id=?',['聂志良','男',22,1522752037000]);
+          tx.executeSql('UPDATE STUDENT SET name = ?,sex = ?,age=? WHERE id=?',[name,sex,age,id]);
         })//动态获取数据
         this.get_data()
-      },
-      close(){
-
+        this.editDialogVisible = false
       }
     }
   }
